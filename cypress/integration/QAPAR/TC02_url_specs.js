05ea2d0ad631cdd53bf99cd9a874451fd399ea55
joseph <josephjohndemarco@gmail.com>
2020-11-06 19:52:32 -0500

    Experimenting with cypress.

--- cypress/integration/QAPAR/TC02_url_specs.js
+++ cypress/integration/QAPAR/TC02_url_specs.js
@@ -1,21 +1,28 @@
-describe('Login', function() {
-  it('Log In', function() {
+describe('Edit profile', function() {
+  it('Visit profile page', function() {
     cy.visit('https://parley-chat.herokuapp.com');
     cy.title().should('eq', 'PARLEY');
     cy.location('protocol').should('eq', 'https:')
-
+  
     cy.get('svg[class="bi bi-arrow-bar-right"]').click();
-
+  
     cy.get('input[name="email"]').type('j@j.com');
-    cy.get('input[name="password"]').type('test');
+    cy.get('input[name="password"]').type('test1');
     cy.get('.btn').contains('Log In').should('be.visible').click();
-
+  
     // Default timeout of about 4 seconds.
     // Supplying optional timeout parameter.
     cy.contains('Signed in as Joseph', {timeout:10000}).should('be.visible');
-  })
+  
+    cy.get('svg[class="bi bi-pen"]').click();
+    cy.contains('Manage Profile').should('be.visible');
 
-  // it('Visit profile', function() {
-  //   cy.get('svg[class="bi bi-pen"]').click();
-  // })
+    cy.url()
+      .should('include', '/profile');
+  });
+  
+  it('test2', function() {
+  
+  });
+
 })
