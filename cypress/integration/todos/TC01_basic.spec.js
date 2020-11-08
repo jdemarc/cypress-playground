ffae4c24e42fb1fc046057426a74bb851d4fada2
joseph <josephjohndemarco@gmail.com>
2020-11-08 17:35:24 -0500

    Implement a few todo tests.

--- cypress/integration/todos/TC01_basic.spec.js
+++ cypress/integration/todos/TC01_basic.spec.js
@@ -1,7 +1,22 @@
 describe("Express Todos App", () => {
-  it("Navigate to page", () => {
-    cy.visit('/');
-    cy.contains('Express To-Do');
-  });
+
   
+  it("Main page", () => {
+    cy.visit('/');
+    cy.contains('Express Todos');
+  });
+
+  it("Go to Todos page", () => {
+    cy.contains('To-Do List').click();
+    cy.url().should('include', '/todos');
+    cy.get('ul[cy-data="todo-list"]').children().should('have.length', 3);
+  });
+
+  it("Delete a Todo", () => {
+    cy.get('button[cy-data="125223"]').click();
+    cy.get('ul[cy-data="todo-list"]').children().should('have.length', 2);
+  });
+
+
+
 });
